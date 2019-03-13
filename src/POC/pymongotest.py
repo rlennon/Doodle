07eddcc77934e4e9e2a6707905539014a909d619
L00144438 <L00144438@student.lyit.ie>
2019-03-13 23:30:18 +0000

    Added command line options

--- src/POC/pymongotest.py
+++ src/POC/pymongotest.py
@@ -1,28 +1,38 @@
+# Doodle PoC Python - MongoDB
+# Usage from command line:
+# python pymongotest.py findall
+# python pymongotest.py add name width length height
+
 import pymongo
 import sys
 
 
-def add(name, width, length, height):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+def dbinfo(collection):
+    # Basic db connection
+    client = pymongo.MongoClient("mongodb://192.168.1.125:27017/")
     db = client["doodletestDB"]
-    col = db["requirement"]
+    col = db[collection]
+    return col
+
+
+def add(name, width, length, height):
+    # Create a new document
+    col = dbinfo("requirement")
     requirement = {"name": name, "width": width, "length": length, "height": height}
     x = col.insert_one(requirement)
     print(x)
 
 
 def findall():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["doodletestDB"]
-    col = db["requirement"]
+    # Print all documents
+    col = dbinfo("requirement")
 
     for x in col.find():
         print(x)
 
 
-# add(*sys.argv[1:])
-# findall()
 def main(args):
+    # Command line arguments
     if args[1] == "add":
         add(*args[2:])
     if args[1] == "findall":
