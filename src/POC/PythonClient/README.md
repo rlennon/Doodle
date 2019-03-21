9665e5286054588976a737e1c840112ea7eaebd7
L00144438 <L00144438@student.lyit.ie>
2019-03-21 21:21:27 +0000

    Updated test client to call each API method Updated Readme

---
+++ src/POC/PythonClient/README.md
@@ -0,0 +1,13 @@
+# Doodle POC Python Client
+
+## Usage
+
+This simple test client, when run, will call each of the API methods in this order
+
+- List all documents in the requirements collection
+- Add a document to the collection
+- Find and print the new document 
+- Add a new field to the ne document
+- Find and print the new document (again)
+- Delete the new document
+- List all the documents in the requirements collection
--- src/POC/PythonClient/pythonAPIclient.py
+++ src/POC/PythonClient/pythonAPIclient.py
@@ -1,26 +1,92 @@
 import requests
+import json
 
-url = "http://192.168.1.125:5000/requirements"
-response = requests.get(url)
-col = response.json()
-# The .json attribute has been deprecated should use the request.get_json() method.
 
-for doc in col:
+url = "http://192.168.0.8:5000/"
+
+
+def requirements_get():
+    print("-" * 75)
+    print("/requirements GET")
+    url1 = url + "requirements"
+    response = requests.get(url1)
+    col = response.json()
+    # The .json attribute has been deprecated should use the request.get_json() method.
+
+    for doc in col:
+        print("Name: {}  ID: {} Width: {} Length: {} Height: {}".format(doc['name'], doc['_id']["$oid"], doc['width']
+                                                                        , doc['length'], doc['height']))
+
+
+def requirement_post():
+    print("-" * 75)
+    print("/requirement POST")
+    url1 = url + "requirement"
+
+    payload = {
+        "width": "11",
+        "name": "Store Room 99",
+        "length": "12",
+        "height": "2"
+    }
+    response = requests.post(url1, json=payload)
+    doc_id = json.loads(response.text)
+    return doc_id["$oid"]
+
+
+def requirement_get(doc_id):
+    print("-" * 75)
+    print("/requirement GET")
+    url1 = url + "requirement"
+    response = requests.get(url1, params={"_Id": doc_id})
+    doc = json.loads(response.text)
+    # This will print the document with the know fields
     print("Name: {}  ID: {} Width: {} Length: {} Height: {}".format(doc['name'], doc['_id']["$oid"], doc['width']
-          , doc['length'], doc['height']))
+                                                                    , doc['length'], doc['height']))
+    # This will print all fields in the document, sorted by key
+    doc_single_line = ""
+    for key, value in sorted(doc.items()):
+        if key == "_id":
+            doc_single_line = doc_single_line + ("ID: {}".format(value["$oid"])) + " "
+        else:
+            doc_single_line = doc_single_line + ("{}: {}".format(key, value)) + " "
+    print(doc_single_line.rstrip())
 
-from flask import Flask, request
-from flask_restful import Resource, Api
-#resp = request.get_json(url)
-#resp1 = request
-#resp1.base_url = url
-#resp = resp1.get_json()
-#request.url = url
-#resp = request.get_json()
-app = Flask(__name__)
-api = Api(app)
 
-@app.route('/requirements')
+def requirement_put(doc_id):
+    print("-" * 75)
+    print("/requirement PUT")
+    url1 = url + "requirement"
+    payload = {
+        "_id": {
+            "$oid": doc_id
+        },
+        "width": "11",
+        "name": "Store Room 99",
+        "length": "12",
+        "height": "2",
+        "new field": "Test"
+    }
+    requests.put(url1, json=payload)
 
-#r = requests.get('http://www.google.com')
-#print(r.text)
+
+def requirement_delete(doc_id):
+    print("-" * 75)
+    print("/requirement DELETE")
+    url1 = url + "requirement"
+    requests.put(url1, params={"_id": doc_id})
+
+
+def main():
+    requirements_get()
+    new_id = requirement_post()
+    requirement_get(new_id)
+    requirement_put(new_id)
+    requirement_get(new_id)
+    requirement_delete(new_id)
+    requirements_get()
+
+
+main()
+
+
