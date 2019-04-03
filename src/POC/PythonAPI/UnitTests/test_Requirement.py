4ddc948a54d40cfd239a13e1e09717c0979bf4f7
L00144438 <L00144438@student.lyit.ie>
2019-04-03 23:43:26 +0100

    Added Unit tests for API (all set to fail or skip). Actual tests need to be coded

---
+++ src/POC/PythonAPI/UnitTests/test_Requirement.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestRequirement(TestCase):
+    def test_post(self):
+        self.fail()  # Fail until test written
+
+    def test_get(self):
+        self.skipTest("TO DO")
+
+    def test_delete(self):
+        self.fail()  # Fail until test written
+
+    def test_put(self):
+        self.fail()  # Fail until test written
---
+++ src/POC/PythonAPI/UnitTests/test_RequirementList.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestRequirementList(TestCase):
+    def test_get(self):
+        self.fail()  # Fail until test written
